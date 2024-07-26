7c49b02254e649e8841678babea2d0e6b09ee953
ozerrrs <ayseozer987@gmail.com>
2024-07-26 14:37:01 +0300

    finitoo

--- full-effect2.js
+++ full-effect2.js
@@ -7,6 +7,38 @@ const fs = require('fs');
 const path = require('path');
 const dir = './video';
 const copyDir = './copy-video'
+const https = require("https");
+const urlArray = [
+    "https://videoai-uploads.univenn.com/94400f5c-428d-43a3-9a1e-92261f611b5a/Gd7w-lOzHHcQW54fNJsMS.mp4",
+    "https://videoai-uploads.univenn.com/f31eabc9-7075-4fd1-af57-65b04d3c41b7/7i0jcH1xx_KmvERcmGFds.mp4",
+    "https://videoai-uploads.univenn.com/a046a008-f02b-4e8b-b05b-6588bbafecee/CNKaPbvBuE28n60LtpEF_.mp4",
+    "https://videoai-uploads.univenn.com/d2979344-63b7-4a36-9155-37905fde54b4/d2BLsjj6JB5QWNByNZcEI.mp4"
+  ];
+  
+  if (!fs.existsSync('./video')) {
+      fs.mkdirSync('./video');
+  }
+  async function urlConvertToArray(url, index) {
+    return new Promise((resolve, reject) => {
+        const filePath = path.resolve(`./video/url${index}.mp4`);
+        const fileStream = fs.createWriteStream(filePath);
+
+        https.get(url, function(res) {
+            res.pipe(fileStream);
+
+            fileStream.on("finish", function() {
+                fileStream.close();
+                console.log(`Downloaded url${index}.mp4`);
+                resolve(filePath);
+            });
+
+            fileStream.on("error", function(err) {
+                fs.unlink(filePath, () => reject(err));
+            });
+        }).on('error', reject);
+    });
+}
+  
 
 async function empty_copy_video() {
     return new Promise((resolve, reject) => {
@@ -237,7 +269,13 @@ async function mergeVideo() {
 
 
 async function main() {
+
+
     try {
+        const downloadPromises = urlArray.map((url, index) => urlConvertToArray(url, index + 1));
+        await Promise.all(downloadPromises);
+        console.log("All videos downloaded");
+
         const emptyMessage = await empty_copy_video();
         console.log(emptyMessage);
 
